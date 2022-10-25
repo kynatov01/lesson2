c1a82e5e9e56743d932e3ae94e50af7cd14be731
Ayan <parkna.ra5798@gmail.com>
2022-10-25 18:02:17 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        String name= "Ayana";
+        int age = 15;
+        final String namePerson= "woolberd";
+        age= 16;
+
+
+        System.out.println(learn());
+
+
+
+
+    }
+
+
+    private static String learn (){
+        return learn();
+
+
+    }
+}
